54a3b240d913a7bda973a025f4545a6247193351
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-10 15:23:40 +0200

    dev: add header

--- front/src/layout/HeaderLayout.vue
+++ front/src/layout/HeaderLayout.vue
@@ -1,9 +1,16 @@
+<script setup lang="ts">
+import { ArrowRightEndOnRectangleIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
-  <header class="flex h-12 items-center px-2 text-white">
+  <header class="flex h-12 items-center justify-between px-2 text-white">
     <RouterLink to="/" class="flex items-center gap-2 hover:underline">
       <img src="/logo.svg" alt="Logo" />
       <span class="text-xl">Gestion Titres</span>
     </RouterLink>
-    <div class="menu"></div>
+    <RouterLink to="/login" class="flex hover:underline">
+      <ArrowRightEndOnRectangleIcon class="size-6" />
+      <span>Se connecter</span>
+    </RouterLink>
   </header>
 </template>
