9baa91e099b71471a773c40c9f04caec23fb062d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-11 10:53:48 +0200

    dev: button

--- front/src/authnz/components/UserInfo.vue
+++ front/src/authnz/components/UserInfo.vue
@@ -8,5 +8,6 @@ const authenticationStore = useAuthenticationStore()
 <template>
   <RouterLink v-if="authenticationStore.user" to="/current-user" class="button layout p-2">
     <UserIcon class="size-6" />
+    <span class="hidden sm:inline">{{ authenticationStore.user.displayName }}</span>
   </RouterLink>
 </template>
