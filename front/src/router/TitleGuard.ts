f66203ac642fd908aed086410e6bb3eb9069b2dc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-13 12:00:44 +0200

    dev: title guard

--- front/src/router/TitleGuard.ts
+++ front/src/router/TitleGuard.ts
@@ -6,13 +6,13 @@ export const titleGuard: NavigationHookAfter = (to) => {
   // Use next tick to handle router history correctly
   // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
   nextTick(() => {
-    const elt = document.querySelector('h1')
-    if (elt !== null) {
-      window.document.title = `${DEFAULT_TITLE} - ${elt.innerText}`
-      return
-    }
     if (typeof to.meta.title === 'string') {
       window.document.title = `${DEFAULT_TITLE} - ${to.meta.title}`
+      return
+    }
+    const elt = document.querySelector<HTMLElement>('main h1')
+    if (elt !== null) {
+      window.document.title = `${DEFAULT_TITLE} - ${elt.innerText}`
       return
     }
     window.document.title = DEFAULT_TITLE
--- front/src/views/WelcomeView.vue
+++ front/src/views/WelcomeView.vue
@@ -11,14 +11,16 @@ const menus = [
 </script>
 
 <template>
-  <div class="page h-full" v-if="authenticationStore.user">
-    <h1>Bienvenue {{ authenticationStore.user.displayName }} !</h1>
-    <p>Vos rôles : Admin, Gérant de portefeuille Truc, Gérant du portefeuille Trac</p>
-    <p>Voici vos modules</p>
-    <nav class="flex flex-wrap gap-2">
-      <RouterLink v-for="item in menus" :key="item.label" :to="item.to" class="button">
-        {{ item.label }}
-      </RouterLink>
-    </nav>
-  </div>
+  <main>
+    <div class="page h-full" v-if="authenticationStore.user">
+      <h1>Bienvenue {{ authenticationStore.user.displayName }} !</h1>
+      <p>Vos rôles : Admin, Gérant de portefeuille Truc, Gérant du portefeuille Trac</p>
+      <p>Voici vos modules</p>
+      <nav class="flex flex-wrap gap-2">
+        <RouterLink v-for="item in menus" :key="item.label" :to="item.to" class="button">
+          {{ item.label }}
+        </RouterLink>
+      </nav>
+    </div>
+  </main>
 </template>
