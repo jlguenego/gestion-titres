a4db50daa0ee178434a83a43ce85a1be22731bc4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-14 15:03:26 +0200

    dev: added privilege view

--- front/src/authnz/views/PrivilegeView.vue
+++ front/src/authnz/views/PrivilegeView.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import { ArrowPathIcon, PlusIcon } from '@heroicons/vue/24/outline'
 import { usePrivilegeStore } from '../stores/PrivilegeStore'
+import { onMounted } from 'vue'
 
 const privilegeStore = usePrivilegeStore()
+
+onMounted(async () => {
+  if (privilegeStore.privileges === undefined) {
+    await privilegeStore.refresh()
+  }
+})
 </script>
 
 <template>
   <main class="grow">
     <div class="page h-full max-w-2xl gap-9">
       <h1>Liste des privileges</h1>
+      <p>
+        Un privilège est un ensemble de fonctionnalités qu'un utilisateur a le droit de se servir si
+        il dispose de ce privilège.
+      </p>
 
       <div class="flex flex-col gap-2">
         <nav class="flex gap-2">
