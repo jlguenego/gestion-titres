67a241254758aa8f3c1dd07350cc89f6cb8e59bb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-15 10:44:34 +0200

    dev: added v-focus as a global directive

--- front/src/widgets/WidgetDeclaration.d.ts
+++ front/src/widgets/WidgetDeclaration.d.ts
@@ -1,7 +1,11 @@
 import LoadingIcon from './components/LoadingIcon.vue'
+import type { vFocus } from './directives/FocusDirective'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     LoadingIcon: typeof LoadingIcon
+  }
+  export interface ComponentCustomProperties {
+    vFocus: typeof vFocus
   }
 }
