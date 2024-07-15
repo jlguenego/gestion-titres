f7ea179e7496e4066d477920afa9f2749a410eb6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-15 16:36:06 +0200

    dev: add menu button

--- front/src/layout/page/HeaderPage.vue
+++ front/src/layout/page/HeaderPage.vue
@@ -1,12 +1,24 @@
 <script lang="ts" setup>
-import { ChevronLeftIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, ChevronLeftIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { useMenuStore } from '../stores/MenuStore'
+
+const menuStore = useMenuStore()
 </script>
 
 <template>
   <header class="flex w-full items-center justify-between bg-black text-white">
-    <button @click="$router.back()" class="bg-transparent text-white">
-      <ChevronLeftIcon class="size-6" />
-    </button>
+    <nav class="flex">
+      <button
+        @click="menuStore.toggleMenu()"
+        class="bg-transparent text-white"
+        v-if="menuStore.showMenu === false"
+      >
+        <Bars3Icon class="size-6" />
+      </button>
+      <button @click="$router.back()" class="bg-transparent text-white">
+        <ChevronLeftIcon class="size-6" />
+      </button>
+    </nav>
     <slot></slot>
     <RouterLink to="/" class="button bg-transparent text-white">
       <XMarkIcon class="size-6" />
