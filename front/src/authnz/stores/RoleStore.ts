e9d8e951a2b6166d15665114b015d7529a218a9a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-16 13:43:35 +0200

    dev: added role view

---
+++ front/src/authnz/stores/RoleStore.ts
@@ -0,0 +1,32 @@
+import type { New } from '@/interfaces/utilities'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { ResourceAPI } from '../api/ResourceAPI'
+import type { Role } from '../interfaces/Role'
+
+const api = new ResourceAPI<Role>('role')
+
+export const useRoleStore = defineStore('privilege', () => {
+  const roles = ref<Role[] | undefined>()
+
+  const refresh = async () => {
+    roles.value = await api.retrieveAll()
+  }
+
+  const add = async (newRole: New<Role>) => {
+    await api.add(newRole)
+    roles.value = await api.retrieveAll()
+  }
+
+  const remove = async (ids: string[]) => {
+    await api.remove(ids)
+    roles.value = await api.retrieveAll()
+  }
+
+  const replace = async (role: Role) => {
+    await api.replace(role)
+    roles.value = await api.retrieveAll()
+  }
+
+  return { roles, refresh, add, remove, replace }
+})
--- front/src/authnz/views/RoleView.vue
+++ front/src/authnz/views/RoleView.vue
@@ -1,27 +1,18 @@
 <script setup lang="ts">
-import { Gender, type User } from '@/authnz/interfaces/User'
-import { useUserStore } from '@/authnz/stores/UserStore'
 import { ArrowPathIcon, PlusIcon } from '@heroicons/vue/24/outline'
+import { useRoleStore } from '../stores/RoleStore'
 import { onMounted } from 'vue'
 
-const userStore = useUserStore()
-
-const getImage = (user: User) => {
-  if (user.gender === undefined || user.gender === Gender.OTHER) {
-    return '/user-nogender.svg'
-  }
-  return user.gender === Gender.MALE ? '/user-male.svg' : '/user-female.svg'
-}
-
+const roleStore = useRoleStore()
 onMounted(async () => {
-  await userStore.refresh()
+  roleStore.refresh()
 })
 </script>
 
 <template>
   <PageLayout class="h-full gap-9">
     <HeaderPage>
-      <h1>Liste des utilisateurs</h1>
+      <h1>Liste des rôles</h1>
     </HeaderPage>
 
     <MainPage>
@@ -34,20 +25,27 @@ onMounted(async () => {
         </RouterLink>
       </nav>
       <div class="flex w-full max-w-md flex-col gap-2">
-        <RouterLink
-          :to="'/users/' + item.username"
-          v-for="item in userStore.users"
-          :key="item.displayName"
+        <div
+          v-for="item in roleStore.roles"
+          :key="item.name"
           class="flex w-full flex-wrap justify-center gap-x-8 gap-y-4 rounded-xl border p-4 shadow-md hover:bg-gray-50 active:shadow-sm"
         >
-          <img :src="getImage(item)" alt="Photo utilisateur" class="h-32 rounded-full" />
           <div class="flex grow flex-col items-center justify-center gap-1">
-            <span class="text-xl">{{ item.displayName }}</span>
-            <span class="font-bold">{{ item.username }}</span>
-            <span>{{ item.jobTitle }}</span>
-            <span class="text-gray-500">{{ item.email }}</span>
+            <span class="text-2xl font-bold">{{ item.name }}</span>
+            <span>{{ item.description }}</span>
+            <span class="flex items-center gap-2 self-start">
+              <span>Privilèges:</span>
+              <RouterLink
+                :to="'/privileges/' + privilege"
+                class="rounded-full border p-2 shadow-md"
+                v-for="privilege in item.privileges"
+                :key="privilege"
+              >
+                {{ privilege }}
+              </RouterLink>
+            </span>
           </div>
-        </RouterLink>
+        </div>
       </div>
     </MainPage>
   </PageLayout>
