be672824a06b57c5541306212c5011c33a68c8be
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-17 18:11:31 +0200

    dev: added disabled mode

--- front/src/widgets/components/SelectItems.vue
+++ front/src/widgets/components/SelectItems.vue
@@ -4,15 +4,24 @@ import { type Item } from '@/interfaces/utilities'
 const model = defineModel<Item[]>({ required: true })
 console.log('model: ', model)
 
-const props = defineProps<{
-  items: Item[]
-}>()
+const props = withDefaults(
+  defineProps<{
+    items: Item[]
+    disabled?: boolean
+  }>(),
+  {
+    disabled: false,
+  },
+)
 
 const isSelectedItem = (item: Item) => {
   return model.value.findIndex((it) => it.id === item.id) !== -1
 }
 
 const toggleItem = async (item: Item) => {
+  if (props.disabled) {
+    return
+  }
   const index = model.value.findIndex((it) => it.id === item.id)
   index !== -1 ? model.value.splice(index, 1) : model.value.push(item)
   model.value = [...model.value]
@@ -20,7 +29,10 @@ const toggleItem = async (item: Item) => {
 </script>
 
 <template>
-  <ul class="flex flex-wrap gap-2 rounded-lg border border-gray-400 p-2">
+  <ul
+    class="flex flex-wrap gap-2 rounded-lg border border-gray-400 p-2"
+    :class="{ 'border-none': props.disabled, 'bg-gray-100': props.disabled }"
+  >
     <li
       v-for="item in props.items"
       :key="item.id"
