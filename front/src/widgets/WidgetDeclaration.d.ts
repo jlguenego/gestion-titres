ce9f6f6e977d51dad45eb131ffa281e263244136
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-17 10:36:51 +0200

    dev: add SelectItems to widgets

--- front/src/widgets/WidgetDeclaration.d.ts
+++ front/src/widgets/WidgetDeclaration.d.ts
@@ -1,9 +1,11 @@
 import LoadingIcon from './components/LoadingIcon.vue'
 import type { vFocus } from './directives/FocusDirective'
+import SelectItems from './components/SelectItems.vue'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     LoadingIcon: typeof LoadingIcon
+    SelectItems: typeof SelectItems
   }
   export interface ComponentCustomProperties {
     vFocus: typeof vFocus
--- front/src/widgets/WidgetPlugin.ts
+++ front/src/widgets/WidgetPlugin.ts
@@ -1,8 +1,10 @@
 import type { Plugin } from 'vue'
 import { vFocus } from './directives/FocusDirective'
 import LoadingIcon from './components/LoadingIcon.vue'
+import SelectItems from './components/SelectItems.vue'
 
 export const widget: Plugin<[]> = (app) => {
   app.directive('focus', vFocus)
   app.component('LoadingIcon', LoadingIcon)
+  app.component('SelectItems', SelectItems)
 }
