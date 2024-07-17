40c9721b493b2b7edffd76a7d83a239c783ac630
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-17 16:46:24 +0200

    dev: added LoadingPage component

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,6 +5,7 @@ import { useUserStore } from './authnz/stores/UserStore'
 import BodyLayout from './layout/BodyLayout.vue'
 import FooterLayout from './layout/FooterLayout.vue'
 import HeaderLayout from './layout/HeaderLayout.vue'
+import LoadingPage from './components/LoadingPage.vue'
 import { sleep } from './utils/misc'
 
 const userStore = useUserStore()
@@ -28,14 +29,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    v-if="isLoading"
-    class="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-green-300"
-  >
-    <span class="text-center text-4xl font-bold">Gestion Titre</span>
-    <span class="text-center text-2xl">La gestion efficace de vos titres !</span>
-    <LoadingIcon class="h-40 w-40 animate-spin" />
-  </div>
+  <LoadingPage v-if="isLoading" />
   <div v-else>
     <div role="none" class="fixed bottom-0 left-0 right-0 top-0 -z-50">
       <img
---
+++ front/src/components/LoadingPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-green-300">
+    <span class="text-center text-4xl font-bold">Gestion Titre</span>
+    <span class="text-center text-2xl">La gestion efficace de vos titres !</span>
+    <LoadingIcon class="h-40 w-40 animate-spin" />
+  </div>
+</template>
