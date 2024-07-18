6169682684e65c09fb25a3ca8cb811d38deb2c2a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-18 16:20:02 +0200

    dev: added hashbang

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,5 @@
 import { authenticationGuard, authenticationRoutes } from '@/authnz/authentication.routes'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 import { NOTFOUND_NAME } from './constants'
@@ -7,7 +7,7 @@ import { responsiveGuard } from './ResponsiveGuard'
 import { titleGuard } from './TitleGuard'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
