83a2013151c31d74a35ee48e2d74098911b7bb5f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-07-20 11:24:21 +0200

    dev: added the VitePWA plugin

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import { Mode, plugin as mdPlugin } from 'vite-plugin-markdown'
+import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,35 @@ export default defineConfig({
       mode: [Mode.VUE],
     }),
     vueDevTools(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'Gestion Titres',
+        short_name: 'Gestion Titres',
+        description: 'Gestion efficace de vos portefeuilles de titres.',
+        theme_color: '#ffffff',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
   ],
   resolve: {
     alias: {
