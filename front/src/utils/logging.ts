caeeb1c733290aa489e8917353279cdb2891c20b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-06 15:50:16 +0200

    dev: added logging

--- front/src/utils/logging.ts
+++ front/src/utils/logging.ts
@@ -1,3 +1,10 @@
+import { useAuthenticationStore } from '@/authnz/stores/AuthenticationStore'
+
 export const log = (...args: unknown[]) => {
-  console.log('%cuser log', 'font-weight: bold', ...args)
+  const authStore = useAuthenticationStore()
+  const now = new Date().toISOString()
+  const user = authStore.user ? authStore.user.displayName : 'not_connected'
+
+  // you can implement this to send data to server.
+  console.log(`%clog`, 'font-weight: bold', now, user, ...args)
 }
