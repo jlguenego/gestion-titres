4a17a86c91d056499889ae60e41d9e288879f7d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-07 15:03:03 +0200

    dev: added css

--- front/src/favorites/views/FavoriteView.vue
+++ front/src/favorites/views/FavoriteView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import type { MenuItem } from '@/authnz/interfaces/Menu'
+import { TrashIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
 
 const favorites = ref<MenuItem[]>([])
+
+const selectedFavorite = ref<MenuItem | undefined>(undefined)
 
 const handleDrop = (event: DragEvent) => {
   console.log('handleDrop event: ', event)
@@ -16,6 +19,18 @@ const handleDrop = (event: DragEvent) => {
 const handleDragOver = (event: Event) => {
   event.preventDefault()
 }
+
+const handleRemove = () => {
+  favorites.value = favorites.value.filter((f) => f.name !== selectedFavorite.value?.name)
+}
+
+const handleSelect = (menuItem: MenuItem) => {
+  if (selectedFavorite.value && selectedFavorite.value.name === menuItem.name) {
+    selectedFavorite.value = undefined
+    return
+  }
+  selectedFavorite.value = menuItem
+}
 </script>
 
 <template>
@@ -27,8 +42,27 @@ const handleDragOver = (event: Event) => {
     <MainPage>
       <p>Glisser - Déposer un élément de menus pour l'ajouter au favoris</p>
       <p>Selectionner un favoris pour l'effacer</p>
-      <div class="h-96 w-full bg-gray-100" @drop="handleDrop" @dragover="handleDragOver">
-        <div v-for="f in favorites" :key="f.name">{{ f.name }}</div>
+      <div class="flex flex-col gap-2">
+        <nav>
+          <button class="button" @click="handleRemove()">
+            <TrashIcon class="size-6" />
+          </button>
+        </nav>
+        <div
+          class="flex h-96 w-full flex-col gap-2 rounded-xl bg-gray-100 p-4"
+          @drop="handleDrop"
+          @dragover="handleDragOver"
+        >
+          <div
+            v-for="f in favorites"
+            :key="f.name"
+            @click="handleSelect(f)"
+            :class="{ selected: selectedFavorite?.name === f.name }"
+            class="bg-white p-2"
+          >
+            {{ f.name }}
+          </div>
+        </div>
       </div>
     </MainPage>
   </PageLayout>
