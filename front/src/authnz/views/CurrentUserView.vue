451cccc19f844560d06e70c3f652ea9c45c9f03b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-09 13:20:38 +0200

    dev: refactor

--- front/src/authnz/views/CurrentUserView.vue
+++ front/src/authnz/views/CurrentUserView.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { getGroups } from '../misc/user'
 import { useAuthenticationStore } from '../stores/AuthenticationStore'
 
 const authenticationStore = useAuthenticationStore()
+const user = authenticationStore.needUser()
 const router = useRouter()
 
 const handleLogout = async () => {
   await authenticationStore.logout()
   await router.replace('/')
 }
+
+const groupList = ref<string>('')
+
+onMounted(async () => {
+  const groups = await getGroups(user)
+  groupList.value = groups.map((g) => g.name).join(', ')
+})
 </script>
 
 <template>
-  <PageLayout class="self-stretch" v-if="authenticationStore.user">
+  <PageLayout class="self-stretch">
     <HeaderPage>
-      <h1>{{ authenticationStore.user.displayName }}</h1>
+      <h1>{{ user.displayName }}</h1>
     </HeaderPage>
     <MainPage>
-      <p>Groupe(s) : Administrateurs</p>
+      <p>
+        Groupe(s) : <span class="font-bold">{{ groupList }}</span>
+      </p>
 
       <button class="button danger" @click="handleLogout()">Se déconnecter</button>
     </MainPage>
