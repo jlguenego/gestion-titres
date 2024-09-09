23a1eaa6118d6fd9789f026dd5afa021975eec9f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-09 15:45:30 +0200

    dev: backend

--- front/src/modules/settings/general/views/BackEndView.vue
+++ front/src/modules/settings/general/views/BackEndView.vue
@@ -6,7 +6,22 @@
       <h1>Mentions Légales</h1>
     </HeaderPage>
     <MainPage class="markdown">
-      <p>Back End View</p>
+      <section>
+        <p>Cette interface peut fonctionner avec plusieurs types de back-end :</p>
+        <ul>
+          <li>
+            IndexedDB: Back-end directement situé dans le navigateur (Base de données IndexedDB)
+          </li>
+          <li>Serveur Web: Back-end stocké situé sur un serveur web distant</li>
+          <li>
+            Fichier: Back-end situé sur le disque dur de l'ordinateur de l'utilisateur (Application
+            seulement)
+          </li>
+        </ul>
+      </section>
+      <section>
+        <p>Back-end : <span class="font-bold">IndexedDB</span></p>
+      </section>
     </MainPage>
   </PageLayout>
 </template>
